30a6ecd8e8074933aebb004774e7e351c608e295
arn4006 <arn4006@g.rit.edu>
2025-02-12 16:12:05 -0500

    feat(Mentat): Added credential checking for SSH

--- README.md
+++ README.md
@@ -6,6 +6,35 @@
 
 Discord bot designed to streamline credential management and access tracking during red team engagements.
 
+## Installation
+
+Mentat is available as a Python package on PyPI and can be installed using `uv` (recommended), `pipx`, or `pip`.
+
+**Note**: python 3.12 is required (currently does not support python 3.13)
+
+Install using uv:
+
+```bash
+uv install tool mentat-bot@latest
+```
+
+Install using pipx:
+
+```bash
+pipx install mentat-bot
+```
+
+Install using pip:
+
+```bash
+pip install mentat-bot
+```
+
+## Features
+
+- Automatic credential tracking for `SSH`
+- Red Team ChatOps via Discord `/` commands.
+
 ## Usage
 
 Mentat is managed using Discord `/` commands.
@@ -13,18 +42,18 @@ Mentat is managed using Discord `/` commands.
 Management commands include:
 
 - `/purge`: Removes all previous host channels
-- `/addhosts`: Adds a list of hosts from an attached newline separated file.
-- `/addcreds <host ip> <username> <password>`: Adds new credentials to a specified host.
+- `/addhosts`: Adds a list of hosts and credentials from an attached `.yaml` file
+- `/addcreds <host ip> <type> <username> <password>`: Adds new credentials to a specified host
 
 ## File formats
 
-Example `hostlist.txt` file:
+Example `hosts.yaml` file:
 
 ```
 192.168.56.1:
   creds:
-    test_user: test_pass
-    hello: world
+    ssh:
+      test_user: test_pass
 
 192.168.56.2
 192.168.56.3
--- mentat/creds.py
+++ mentat/creds.py
@@ -3,82 +3,7 @@ from discord.ext import tasks, commands
 import logging
 import re
 import yaml
-
-
-class CredManagement(commands.Cog):
-    """
-    CreManagement contains the set of commands used for credential management.
-    """
-
-    def __init__(self, bot):
-        self.bot = bot
-        self.logger = logging.getLogger("discord")
-        self.hosts: list[Host] = []
-        self.updatecreds.start()
-
-    @commands.command()
-    async def addcreds(self, ctx, ip: str, username: str, password: str):
-        """
-        Add creds to a host
-        """
-        try:
-            for host in self.hosts:
-                host_ip = host.get_ip()
-                if host_ip == ip.replace(".", "_"):
-                    new_creds = Credentials(username, password)
-                    self.logger.info(f"{host_ip}: adding creds: {new_creds}")
-                    host.add_creds(new_creds)
-        except Exception as e:
-            self.logger.error(e)
-
-    @commands.command()
-    async def addhosts(self, ctx):
-        """
-        Adds hosts to mentat via a newline separated list
-        """
-        try:
-            for attachment in ctx.message.attachments:
-                self.logger.info(f"adding hosts from: {attachment.filename}")
-                content = await attachment.read()
-                hostlist = yaml.safe_load(content)
-                for ip, data in hostlist.items():
-                    # sanitize for Discord channel name conventions
-                    ip = ip.replace(".", "_")
-                    if ip not in self.hosts:
-                        channel = await ctx.guild.create_text_channel(ip)
-                        new_host = Host(channel)
-                        # add all host creds
-                        if data:
-                            for username, password in data["creds"].items():
-                                self.logger.info(username)
-                                self.logger.info(password)
-                                new_creds = Credentials(username, password)
-                                new_host.add_creds(new_creds)
-                        self.hosts.append(new_host)
-        except Exception as e:
-            self.logger.error(e)
-
-    @tasks.loop(seconds=5.0)
-    async def updatecreds(self):
-        """
-        updatecreds is a background task that updates all known credentials for each host in Discord.
-        """
-        self.logger.info("updating creds")
-        for host in self.hosts:
-            await host.update_creds()
-
-    @commands.command()
-    async def purge(self, ctx):
-        """
-        purge removes all previous host channels. Use on mentat startup
-        """
-        self.logger.info("purging channels")
-        try:
-            for channel in self.bot.get_all_channels():
-                if re.match(r"^[0-9]+_[0-9]+_[0-9]+_[0-9]+$", channel.name):
-                    await channel.delete()
-        except Exception as e:
-            self.logger.error(e)
+import paramiko
 
 
 class Credentials:
@@ -86,19 +11,19 @@ class Credentials:
     Credentials contains the data for a set of creds.
     """
 
-    def __init__(self, username: str, password: str, type: str):
+    def __init__(self, pass_type: str, username: str, password: str):
+        self.pass_type: str = pass_type
         self.username: str = username
         self.password: str = password
-        self.type: str = type
 
     def __repr__(self) -> str:
-        return f"{type}: username: {self.username}, password: {self.password}"
+        return f"type: {self.pass_type}, username: {self.username}, password: {self.password}"
 
     def __eq__(self, other) -> bool:
         return (
-            self.username == other.username
+            self.pass_type == other.pass_type
+            and self.username == other.username
             and self.password == other.password
-            and self.type == other.type
         )
 
 
@@ -118,6 +43,12 @@ class Host:
         """
         self.credentials.append(creds)
 
+    def remove_creds(self, creds: Credentials):
+        """
+        remove_creds removes creds from the host.
+        """
+        self.credentials.remove(creds)
+
     async def update_creds(self):
         """
         update_creds pushes all credential changes to the host channel in Discord.
@@ -126,7 +57,7 @@ class Host:
         msg = "Current working credentials:\n"
         if self.credentials:
             for creds in self.credentials:
-                msg += f"- `{creds.username}`: `{creds.password}`\n"
+                msg += f"- **{creds.pass_type.upper()}**: `{creds.username}`:`{creds.password}`\n"
         else:
             msg += "none :("
 
@@ -139,4 +70,115 @@ class Host:
             await self.creds_msg.edit(content=msg)
 
     def get_ip(self) -> str:
-        return self.channel.name
+        return self.channel.name.replace("_", ".")
+
+
+class CredManagement(commands.Cog):
+    """
+    CreManagement contains the set of commands used for credential management.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger("discord")
+        self.hosts: list[Host] = []
+        self.updatecreds.start()
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    @tasks.loop(seconds=5.0)
+    async def updatecreds(self):
+        """
+        updatecreds is a background task that updates all known credentials for each host in Discord.
+        """
+        self.logger.info("updating creds")
+        for host in self.hosts:
+            self.checkcreds(host)
+            await host.update_creds()
+
+    def checkcreds(self, host: Host):
+        """
+        checkcreds validates that all host credentials are working.
+        """
+        self.logger.info("checking creds")
+        for cred in host.credentials:
+            if not self.cred_is_working(host.get_ip(), cred):
+                host.remove_creds(cred)
+
+    def cred_is_working(self, ip: str, cred: Credentials) -> bool:
+        """
+        cred_is_working validates credentials by invoking each credential types specific checker.
+        """
+        match cred.pass_type:
+            case "ssh":
+                return self.check_ssh(ip, cred)
+            case _:
+                return False
+
+    def check_ssh(self, ip: str, cred: Credentials) -> bool:
+        """
+        check_ssh attempts an SSH connection using the specified credentials against the target, and returns True if the attempt is successful.
+        """
+        try:
+            self.ssh.connect(
+                ip, port=2222, username=cred.username, password=cred.password
+            )
+            return True
+        except Exception as e:
+            self.logger.error(e)
+            return False
+
+    @commands.command()
+    async def purge(self, ctx):
+        """
+        purge removes all previous host channels. Use on mentat startup
+        """
+        self.logger.info("purging channels")
+        try:
+            for channel in self.bot.get_all_channels():
+                if re.match(r"^[0-9]+_[0-9]+_[0-9]+_[0-9]+$", channel.name):
+                    await channel.delete()
+        except Exception as e:
+            self.logger.error(e)
+
+    @commands.command()
+    async def addcreds(self, ctx, type: str, ip: str, username: str, password: str):
+        """
+        Add creds to a host
+        """
+        try:
+            for host in self.hosts:
+                if ip == host.get_ip():
+                    new_creds = Credentials(type, username, password)
+                    self.logger.info(f"{ip}: adding creds: {new_creds}")
+                    host.add_creds(new_creds)
+        except Exception as e:
+            self.logger.error(e)
+
+    @commands.command()
+    async def addhosts(self, ctx):
+        """
+        Adds hosts to mentat via a newline separated list
+        """
+        try:
+            for attachment in ctx.message.attachments:
+                self.logger.info(f"adding hosts from: {attachment.filename}")
+                content = await attachment.read()
+                hostlist = yaml.safe_load(content)
+                for ip_addr, data in hostlist.items():
+                    # sanitize for Discord channel name conventions
+                    santitized_ip = ip_addr.replace(".", "_")
+                    if santitized_ip in self.hosts:
+                        continue
+
+                    channel = await ctx.guild.create_text_channel(santitized_ip)
+                    new_host = Host(channel)
+                    # add all host creds
+                    if data:
+                        for pass_type, creds_block in data["creds"].items():
+                            for username, password in creds_block.items():
+                                new_creds = Credentials(pass_type, username, password)
+                                new_host.add_creds(new_creds)
+                    self.hosts.append(new_host)
+        except Exception as e:
+            self.logger.error(e)
